b370e6683f7df912614a9e28fb8b6add4d997499
Collin Sakuma <collinmsakuma@gmail.com>
2023-01-30 16:46:01 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const saturdayFun = (activity ="roller-skate") => {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = (activity ="go to the office") => {
+    return `This Monday, I will ${activity}.`;
+}
+
+const wrapAdjective = (var1) => {
+    return function (var2 = 'special') {
+        return `You are ${var1}${var2}${var1}!`;
+    }
+}
+
+// let result = wrapAdjective('*');
+// let emphatic = result('a hard worker');
+// console.log(emphatic);
